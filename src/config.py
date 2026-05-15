acd8f0d59d75b857c4bd1116cd096efa24c7da49
mrbateuk <thanhtuan.to1993@gmail.com>
2026-05-15 23:11:39 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ def get_env(key: str) -> str:
 
 class Config:
     telegram_token: str = os.getenv("TELEGRAM_TOKEN", "")
-    gemini_api_key: str = os.getenv("GEMINI_API_KEY", ""
+    gemini_api_key: str = os.getenv("GEMINI_API_KEY", "")
     serpapi_key: str = os.getenv("SERPAPI_KEY", "")
     allowed_user_id: str = os.getenv("ALLOWED_USER_ID", "")
     affiliate_template = os.getenv("AFFILIATE_TEMPLATE")
